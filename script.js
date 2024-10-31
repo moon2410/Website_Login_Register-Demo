c691b956fe2cd186237085f12c3ec89efd23aeb4
Moonsatul Islam Moon <123303628+moon2410@users.noreply.github.com>
2024-10-31 19:24:01 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Handle register form submission
+document.getElementById('register-form')?.addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Successfully registered!');
+});
+
+// Handle login form submission
+document.getElementById('login-form')?.addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Logged in successfully!');
+});
